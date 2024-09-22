b1dd96628dcbec39df54f0a0f09460498c76212d
mrwendellbarros <wendell7gabriel@gmail.com>
2024-09-22 18:23:10 -0300

    first

---
+++ classes.kt
@@ -0,0 +1,15 @@
+class Customer                                  // 1
+
+class Contact(val id: Int, var email: String)   // 2
+
+fun main() {
+
+    val customer = Customer()                   // 3
+    
+    val contact = Contact(1, "mary@gmail.com")  // 4
+
+    println(contact.id)                         // 5
+    println(contact.email)
+    contact.email = "jane@gmail.com"            // 6
+    println(contact.email)
+}
---
+++ hello.kt
@@ -0,0 +1,5 @@
+package me.dio.helloworld           // 1. Definição do pacote, algo muito comum para organização de código (apesar de opcional).
+
+fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
+    println("Olá, Mundo!")          // 3. Imprime na saída padrão um texto usando a função [println] (importada implicitamente).
+}
