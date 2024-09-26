3e911c9a2bf968f5f561ae77e4bf67fa8620965b
mrwendellbarros <wendell7gabriel@gmail.com>
2024-09-26 17:06:50 -0300

    suspend function

---
+++ suspendFun.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.*
+
+
+fun main() = runBlocking {
+    doWorld()
+}
+
+suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    print("Hello ")
+}
